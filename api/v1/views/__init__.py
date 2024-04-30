efbb62ae239419800aa28bd7fc596bdab0422649
BeautifulAngel246 <favouronuche@gmail.com>
2024-04-30 04:33:09 -0700

    done finals

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
+# api/v1/views/__init__.py
+
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
+# Import all necessary views
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -10,3 +11,6 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
+
+# Create the Blueprint object
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
