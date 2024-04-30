550424e4efe83e184b012ec7e7fe389e2b598663
BeautifulAngel246 <favouronuche@gmail.com>
2024-04-30 04:44:23 -0700

    underscore

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,11 @@
-# api/v1/views/__init__.py
-
+#!/usr/bin/python3
+"""
+This package contains the view endpoints for the AirBnB clone API.
+"""
 from flask import Blueprint
 
-# Import all necessary views
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -11,6 +14,3 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
-
-# Create the Blueprint object
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
